b29dac0080a268b0b0de7413b2b8b5f22c114d9b
tsukasa <tsukasak4991@gmail.com>
2022-01-25 16:09:34 +0900

    1/25

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,33 @@
 "use strict";
+//npx tsc > (ls dist >) node dist/index.js　
 function hello(user) {
     return `hello, ${user}`;
 }
-console.log(hello("typescript"));
+// console.log(hello("typescript"));
+let fruits = ['apple', 'orange', 'grape'];
+function juicer(fruit) {
+    return fruit + "juice";
+}
+let hiiragi;
+hiiragi = {
+    clientName: 'ひいらぎ不動産株式会社',
+    phoneNumber: '03-XXXX-XXXX'
+};
+// console.log(hiiragi);
+var FoodCategory;
+(function (FoodCategory) {
+    FoodCategory[FoodCategory["MEAT"] = 0] = "MEAT";
+    FoodCategory[FoodCategory["FISH"] = 1] = "FISH";
+    FoodCategory[FoodCategory["VEGETABLE"] = 2] = "VEGETABLE";
+    FoodCategory[FoodCategory["FRUITS"] = 3] = "FRUITS";
+})(FoodCategory || (FoodCategory = {}));
+const apple = {
+    scientificName: 'Malus domestica',
+    category: FoodCategory.FRUITS
+};
+// console.log(FoodCategory[0]);
+// console.log(apple);
+let address = '';
+address = '東京都港区芝' + 1 + '丁目';
+console.log(address);
 //# sourceMappingURL=index.js.map
